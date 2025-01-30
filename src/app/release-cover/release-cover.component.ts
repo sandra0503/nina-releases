56f837943d3fac719b70a383a98c242ac673b39e
Sandra Zollner <sandra.b.zollner@gmail.com>
2025-01-30 12:52:59 +0100

    feat: adds wavesurfer and enables basic play

--- src/app/release-cover/release-cover.component.ts
+++ src/app/release-cover/release-cover.component.ts
@@ -1,13 +1,15 @@
-import { Component, Input } from '@angular/core';
-import { ReleaseMetadata } from '../release';
+import { Component, Input } from "@angular/core";
+import { ReleaseMetadata } from "../release";
+import { CommonModule } from "@angular/common";
 
 @Component({
-  selector: 'app-release-cover',
-  imports: [],
-  templateUrl: './release-cover.component.html',
-  styleUrl: './release-cover.component.scss',
+  selector: "app-release-cover",
+  imports: [CommonModule],
+  templateUrl: "./release-cover.component.html",
+  styleUrl: "./release-cover.component.scss",
 })
 export class ReleaseCoverComponent {
   @Input() release!: ReleaseMetadata;
   @Input() publisher!: string;
+  @Input() isActive!: boolean;
 }
